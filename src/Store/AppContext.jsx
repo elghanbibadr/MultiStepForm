24b50f21594d6d6b1716e99156cdd097f53f6c8d
badr ghanbi <bghanbi50@gmail.com>
2022-12-28 20:40:49 +0100

    create the context componnent

---
+++ src/Store/AppContext.jsx
@@ -0,0 +1,16 @@
+import { createContext,useState } from "react";
+
+export const AppContext=createContext(null);
+
+export const AppContextProvider=({children})=>{
+    const [stepNumber,setStepNumber]=useState(1);
+
+
+    const value={
+        stepNumber,
+        setStepNumber
+    }
+
+ return <AppContext.Provider value={value} >{children}</AppContext.Provider>
+
+}
