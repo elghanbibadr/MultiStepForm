82216cfdb385a7675a6514307c720ae035cf35a2
badr ghanbi <bghanbi50@gmail.com>
2022-12-30 11:40:02 +0100

    add basic styles for the personal info componenet

--- src/Componenet/StepsContent/PersonalInfo/PersonalInfo.jsx
+++ src/Componenet/StepsContent/PersonalInfo/PersonalInfo.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
-
 const PersonalInfo = () => {
   return (
-    <div className=' personalInfo'>
-        <h1>PersonalInfo</h1>
-    </div>
+    <>
+      <h2 className='PersonalInfo-title'>Personal Info</h2>
+      <p>Please provide your name, email address, and phone number.</p>
+    <from className=' personalInfo'>
+       <div className="from-controller">
+        <label htmlFor="userName">Name</label>
+        <input id='userName' type="text" placeholder='e.g.Stephen King' />
+        </div>
+       <div className="from-controller">
+        <label htmlFor="email">Email Address</label>
+        <input id='email' type="email" placeholder='e.g.stephenKing@lorem.com' />
+        </div>
+       <div className="from-controller">
+        <label htmlFor="userName">Phone Number</label>
+        <input id='userName' type="text" placeholder='e.g. 1234567890' />
+        </div>
+    </from>
+    
+    </>
   )
 }
 
