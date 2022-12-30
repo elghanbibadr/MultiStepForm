fb49869c4af7ea13c7832f59950d28efa25029d4
badr ghanbi <bghanbi50@gmail.com>
2022-12-30 09:53:31 +0100

    clear up the app context and ommit the unused states

--- src/Componenet/paginationBar/PaginationBar.jsx
+++ src/Componenet/paginationBar/PaginationBar.jsx
@@ -1,28 +1,27 @@
-import React,{useContext, useEffect} from 'react'
+import React, { useContext, useEffect } from 'react'
 import { AppContext } from '../../Store/AppContext';
 import Button from '../General/Button'
 
 const PaginationBar = () => {
-    const {stepNumber,setStepNumber,currentPageVisibleContent,setCurrentPageVisibleContent,  personalInfoIndex, addOnsIndex,  selectPlanIndex, SummaryIndex}=useContext(AppContext);
-    const handleBtnClicked=(e)=>{
-        if(e.target.id==='nextstep' || e.target.id==="confirm"){
-          setStepNumber(prv => prv + 1);
-      
-        }
-      
-      }
-
-   
+  const { stepNumber, setStepNumber} = useContext(AppContext);
+  const handleBtnClicked = (e) => {
+    if (e.target.id === 'nextstep' || e.target.id === "confirm") {
+      setStepNumber(prv => prv + 1);
+    }
+  }
 
 
-      
-     
+  const handleBackLinkClicked = () => setStepNumber(prv => prv - 1)
+
+
+
+
 
   return (
     <div className='paginationBar'>
-    { (stepNumber>=2 && stepNumber<5) && <a className='back-link'  >Go Back</a> }
-   {  stepNumber<4 && <Button id='nextstep' handleBtnClicked={handleBtnClicked} btnClassName='btn--marineBlue' name='Next Step' />}
- { (stepNumber>=4 && stepNumber<5) && <Button id='confirm' handleBtnClicked={handleBtnClicked} btnClassName='btn--Purplishblue' name='Confirm' />}
+      {(stepNumber >= 2 && stepNumber < 5) && <a onClick={handleBackLinkClicked} className='back-link'  >Go Back</a>}
+      {stepNumber < 4 && <Button id='nextstep' handleBtnClicked={handleBtnClicked} btnClassName='btn--marineBlue' name='Next Step' />}
+      {(stepNumber >= 4 && stepNumber < 5) && <Button id='confirm' handleBtnClicked={handleBtnClicked} btnClassName='btn--Purplishblue' name='Confirm' />}
     </div>
   )
 }
--- src/Store/AppContext.jsx
+++ src/Store/AppContext.jsx
@@ -1,31 +1,16 @@
-import { createContext,useState } from "react";
+import { createContext, useState } from "react";
 
-export const AppContext=createContext(null);
+export const AppContext = createContext(null);
 
-export const AppContextProvider=({children})=>{
-    const [stepNumber,setStepNumber]=useState(1);
-    const [personalInfoIndex,addOnsIndex,selectPlanIndex,SummaryIndex]=[1,2,3,4];
-
-    
-    const [currentPageVisibleContent,setCurrentPageVisibleContent]=useState({
-        1:true,
-        2:false,
-        3:false,
-        4:false,
-    })
+export const AppContextProvider = ({ children }) => {
+    const [stepNumber, setStepNumber] = useState(1);
 
 
-    const value={
+    const value = {
         stepNumber,
         setStepNumber,
-        currentPageVisibleContent,
-        setCurrentPageVisibleContent,
-        personalInfoIndex,
-        addOnsIndex,
-        selectPlanIndex,
-        SummaryIndex,
     }
 
- return <AppContext.Provider value={value} >{children}</AppContext.Provider>
+    return <AppContext.Provider value={value} >{children}</AppContext.Provider>
 
 }
